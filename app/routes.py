777938976ea48f1c63c840297a07ce45e33790eb
mrthere3 <871780968@qq.com>
2021-02-10 20:42:54 +0800

    templates finish 第二章完成

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,22 @@
 # @File :routes.py
 # @Software：PyCharm
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello ,World"
+    user = {'username': 'Miguel'}
+    posts = [  # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+    {
+        'author': {'username': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+    }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
--- microblog.py
+++ microblog.py
@@ -1,9 +1,9 @@
-#-*- codeing = utf-8 -*- 
-#@Time : 2021/2/10 14:14
-#@Author :王小刚
-#@File :microblog.py
-#@Software：PyCharm
+# -*- codeing = utf-8 -*-
+# @Time : 2021/2/10 14:14
+# @Author :王小刚
+# @File :microblog.py
+# @Software：PyCharm
 from app import app
+
 if __name__ == '__main__':
     app.run(debug=True)
-    print("第二次提交")
