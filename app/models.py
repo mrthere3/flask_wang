fd1d698eef73cfa46277ef1b337a0ac2b2098e3f
mrthere3 <871780968@qq.com>
2021-02-11 17:37:33 +0800

    add database user and post

---
+++ app/models.py
@@ -0,0 +1,30 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2021/2/11 15:34
+# @Author :王小刚
+# @File :models.py
+# @Software：PyCharm
+from datetime import datetime
+
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+# post和user是多对一的关系,所以在多的那一边设置外键user_id 在一的一方设置主键
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}'.format(self.body)
